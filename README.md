658f8e33e48a0940ae8ce4bf8588d739a8d1ae00
leon-do <leondo123@gmail.com>
2022-08-13 01:50:39 -0400

    🤞🏼

---
+++ README.md
@@ -0,0 +1,34 @@
+# Bipsea subgraph
+
+## Subgraphs
+
+https://api.studio.thegraph.com/query/5210/bipsea-goerli/v0.0.1/graphql?query=query+MyQuery+%7B%0A++balances%28first%3A+10%29+%7B%0A++++address%0A++++balance%0A++%7D%0A++items+%7B%0A++++blockNumber%0A++++canBuy%0A++++itemId%0A++++investor%0A++++price%0A++++seller%0A++++sold%0A++++timestamp%0A++++transactionHash%0A++++uri%0A++%7D%0A++purchases%28first%3A+10%29+%7B%0A++++amount%0A++++blockNumber%0A++++buyer%0A++++itemId%0A++++timestamp%0A++++transactionHash%0A++%7D%0A++withdrawals%28first%3A+10%29+%7B%0A++++amount%0A++++blockNumber%0A++++timestamp%0A++++to%0A++++transactionHash%0A++%7D%0A%7D
+
+## Build and Deploy
+
+```bash
+yarn codegen && yarn build --network goerli && yarn deploy:goerli 
+```
+
+## Adding Networks
+
+### Create a Subgraph.
+
+![](https://user-images.githubusercontent.com/19412160/184470320-43c0ce0c-3772-4f11-b47d-17b56c905a6c.png)
+
+### Call it `bipsea-<NETWORK_NAME>`
+
+![](https://user-images.githubusercontent.com/19412160/184470333-9fe683d8-8774-4db6-af37-31f6ab89b60d.png)
+
+### Deploy contract and get block number
+
+![](https://user-images.githubusercontent.com/19412160/184470286-ed796dea-c488-4c58-a873-f19280f1d526.png)
+
+### In `networks.json`, add `address` and `startBlock` number
+
+![](https://user-images.githubusercontent.com/19412160/184470382-d1fb070d-5f6a-4fc9-87f1-ad152e807cc1.png)
+
+### In `package.json` add `deploy:<NETWORK_NAME>`
+
+![](https://user-images.githubusercontent.com/19412160/184470435-e88c1c73-a634-4d4e-bc7c-33ebda351d4e.png)
+
--- src/bipsea.ts
+++ src/bipsea.ts
@@ -1,62 +1,91 @@
-import { BigInt } from "@graphprotocol/graph-ts"
-import {
-  Bipsea,
-  Buy,
-  Delist,
-  Relist,
-  Sell,
-  Withdraw
-} from "../generated/Bipsea/Bipsea"
-import { ExampleEntity } from "../generated/schema"
+import { BigInt } from "@graphprotocol/graph-ts";
+import { Bipsea, Buy, Sell, Withdraw, Delist, Relist } from "../generated/Bipsea/Bipsea";
+import { Item, Purchase, Withdrawal, Balance } from "../generated/schema";
 
 export function handleBuy(event: Buy): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+  let buy = new Purchase(event.params._itemId.toHexString() + "-" + event.params._buyer.toHexString());
+  buy.buyer = event.params._buyer;
+  buy.itemId = event.params._itemId;
+  buy.amount = event.params._amount;
+  buy.transactionHash = event.transaction.hash;
+  buy.blockNumber = event.block.number;
+  buy.timestamp = event.block.timestamp;
+  buy.save();
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+  let item = Item.load(event.params._itemId.toHexString());
+  if (item != null) {
+    item.sold = item.sold.plus(BigInt.fromI32(1));
+    item.save();
   }
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
+  let sellerAddress = Bipsea.bind(event.address).items(event.params._itemId).value0;
+  let seller = Balance.load(sellerAddress.toHexString());
+  if (seller == null) {
+    seller = new Balance(sellerAddress.toHexString());
+    seller.address = sellerAddress;
+  }
+  seller.balance = Bipsea.bind(event.address).balances(sellerAddress);
+  seller.save();
 
-  // Entity fields can be set based on event parameters
-  entity._itemId = event.params._itemId
-  entity._buyer = event.params._buyer
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.balances(...)
-  // - contract.items(...)
-  // - contract.purchase(...)
+  let investorAddress = Bipsea.bind(event.address).items(event.params._itemId).value1;
+  let investor = Balance.load(investorAddress.toHexString());
+  if (investor == null) {
+    investor = new Balance(investorAddress.toHexString());
+    investor.address = investorAddress;
+  }
+  investor.balance = Bipsea.bind(event.address).balances(investorAddress);
+  investor.save();
 }
 
-export function handleDelist(event: Delist): void {}
+export function handleSell(event: Sell): void {
+  let sell = new Item(event.params._itemId.toHexString());
+  sell.itemId = event.params._itemId;
+  sell.seller = Bipsea.bind(event.address).items(event.params._itemId).value0;
+  sell.investor = Bipsea.bind(event.address).items(event.params._itemId).value1;
+  sell.uri = Bipsea.bind(event.address).items(event.params._itemId).value2;
+  sell.price = Bipsea.bind(event.address).items(event.params._itemId).value3;
+  sell.canBuy = true;
+  sell.transactionHash = event.transaction.hash;
+  sell.blockNumber = event.block.number;
+  sell.timestamp = event.block.timestamp;
+  sell.sold = BigInt.fromI32(0);
+  sell.save();
+}
 
-export function handleRelist(event: Relist): void {}
+export function handleWithdraw(event: Withdraw): void {
+  let sellerWithdraw = new Withdrawal(event.transaction.hash.toHex() + "-" + event.params._sellerAddress.toHexString() + "-" + event.params._sellerAmount.toHexString());
+  sellerWithdraw.to = event.params._sellerAddress;
+  sellerWithdraw.amount = event.params._sellerAmount;
+  sellerWithdraw.transactionHash = event.transaction.hash;
+  sellerWithdraw.blockNumber = event.block.number;
+  sellerWithdraw.timestamp = event.block.timestamp;
+  sellerWithdraw.save();
 
-export function handleSell(event: Sell): void {}
+  let sellerAddress = Balance.load(event.params._sellerAddress.toHexString());
+  if (sellerAddress != null) {
+    sellerAddress.balance = Bipsea.bind(event.address).balances(event.params._sellerAddress);
+    sellerAddress.save();
+  }
 
-export function handleWithdraw(event: Withdraw): void {}
+  let withdrawerAddress = Balance.load(event.params._withdrawerAddress.toHexString());
+  if (withdrawerAddress != null) {
+    withdrawerAddress.balance = Bipsea.bind(event.address).balances(event.params._withdrawerAddress);
+    withdrawerAddress.save();
+  }
+}
+
+export function handleDelist(event: Delist): void {
+  let item = Item.load(event.params._itemId.toHexString());
+  if (item != null) {
+    item.canBuy = false;
+    item.save();
+  }
+}
+
+export function handleRelist(event: Relist): void {
+  let item = Item.load(event.params._itemId.toHexString());
+  if (item != null) {
+    item.canBuy = true;
+    item.save();
+  }
+}
